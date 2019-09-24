a14042712d4cd23ae87dd7d019ae57cfaf1cabd2
LeaCollin0518 <leac750@gmail.com>
2019-09-24 17:09:28 -0400

    add bash script to download epa data

---
+++ download_epa_data.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+__DIR__="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")" # directory of this script
+mkdir -p "${__DIR__}/epa_data"
+cd "${__DIR__}/epa_data"
+
+epa_url='https://aqs.epa.gov/aqsweb/airdata/daily_88101_'
+
+for i in `seq 2000 2016`; do
+	curl -OL "${epa_url}$i.zip"
+
+done
+
+for zipfile in *zip; do
+	newdir="${zipfile%.zip}"
+
+	unzip "${zipfile}" -d "${newdir}"
+	rm "${zipfile}"
+
+	cd "${__DIR__}/epa_data/${newdir}"
+	mv *.csv "${__DIR__}/epa_data"
+	cd "${__DIR__}/epa_data"
+	rm -rf "${__DIR__}/epa_data/${newdir}"
+done
