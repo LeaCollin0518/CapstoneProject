c286c4a202a9980a7ca12ce8821c37a248195581
LeaCollin0518 <leac750@gmail.com>
2019-09-30 18:02:22 -0400

    make CA point graphs

--- mapping_epa_2016.R
+++ mapping_epa_2016.R
@@ -16,6 +16,13 @@ library(dismo)
 library(ggplot2)
 library(maps)
 library(mapdata)
+library(sp)
+library(gstat)
+library(automap)
+require(dplyr); require(RColorBrewer); require(ggplot2)
+require(mapdata); require(maptools)
+library(raster) 
+library(dismo)
 
 library(data.table)
 
@@ -63,8 +70,10 @@ epa_2016$X <- NULL # delete dummy index column
 epa_2016$Date.Local <- as.Date(epa_2016$Date.Local)
 num.columns <- c('POC', 'Latitude', 'Longitude', 'Arithmetic.Mean')
 epa_2016[num.columns] <- sapply(epa_2016[num.columns], as.numeric)
-ca_2016 <- epa_2016 %>% filter(State.Code == '06')
 
+### MAPPING ONLY CA COUNTIES (JAN 2016)
+
+ca_2016 <- epa_2016 %>% filter(State.Code == '06')
 ca_fips <- fips_codes %>% filter(State.Code == '06')
 ca_fips$Area.Name <- sapply(ca_fips$Area.Name, tolower)
 ca_fips <- ca_fips %>% filter(County.Code != '000')
@@ -110,165 +119,46 @@ ditch_the_axes <- theme(
   axis.title = element_blank()
 )
 
-pal <- leaflet::colorNumeric(rev(brewer.pal(n=11, name = "RdYlGn")), ca_pm25$mean_pm2.5,
-                    na.color = "transparent")
-
 elbow_room1 <- ca_base + 
   geom_polygon(data = ca_pm25, aes(fill = mean_pm2.5), color = "white") +
   geom_polygon(color = "black", fill = NA) +
-  scale_fill_gradientn(colours = terrain.colors(10)) +
+  scale_fill_gradientn(colours = terrain.colors(20)) +
   theme_bw() +
   ditch_the_axes
 
 elbow_room1
 
-### CHICAGO
+### TRYING MORE STUFF
+ca.jan.01.2016 <- ca_epa %>% filter(Date.Local == '2016-01-01')
+coordinates(ca.jan.01.2016) <- ~ Longitude + Latitude
 
-r = raster("plotting/GWR_PM25_NA_200501_200712-RH35-NoNegs.asc/GWR_PM25_NA_200501_200712-RH35-NoNegs.asc")
-r
+states <- rgdal::readOGR("plotting/tl_2017_us_state/tl_2017_us_state.shp")
+states <- states[states$STUSPS %in% c('CA'),] 
+bbox(ca.jan.01.2016)
 
-class(r)
-dim(r)
+mycol.palette <- colorRampPalette(c("blue", "green","yellow",  "orange", "red"), space = "rgb")
 
-shape_file <- "gz_2010_17_140_00_500k"
-shape_file_dir <- paste0("plotting/", shape_file)
-raw_tract <- readOGR(dsn = shape_file_dir, layer = shape_file)
+ca.jan.01.2016 %>% as.data.frame %>% 
+  ggplot() + geom_polygon(data = states, 
+                          aes(x=long, y = lat, group = group), 
+                          fill = "white", 
+                          color="black") +
+  coord_fixed(xlim = c(-124.18, -115.49),  ylim = c(32.64, 41.73), ratio = 1.1) +
+  geom_point(aes(x = Longitude, y = Latitude, color=Arithmetic.Mean), size = 3, alpha=3/4) + 
+  scale_colour_gradientn(colours = terrain.colors(20)) +
+  ggtitle("True PM 2.5") + coord_equal() + theme_bw()
 
-pmdat <- projectRaster(r, crs = crs(raw_tract))
-# crop the data
-pmdat <- crop(pmdat, raw_tract)
+ca_epa_jan <- ca_epa %>% filter(month(Date.Local) == '1') %>% dplyr::select(Site.Num, Latitude, Longitude, Arithmetic.Mean, subregion)
+ca_epa_jan <- data.frame(aggregate(ca_epa_jan$Arithmetic.Mean, list(ca_epa_jan$Latitude, ca_epa_jan$Longitude), mean))
+names(ca_epa_jan) <- c('Latitude', 'Longitude', 'PM2.5')
+coordinates(ca_epa_jan) <- ~ Longitude + Latitude
 
-class(pmdat)
-pmdat
-head(coordinates(pmdat)) # x = longitude, y = latitude
-head(values(pmdat))
-summary(values(pmdat))
-summary(values(pmdat)[which(values(pmdat)>0)])
-
-pmdat2 <- raster::extract(pmdat, # the raster that you wish to extract values from
-                          raw_tract, # a point, or polygon spatial object
-                          buffer = .1, # specify a .5 degree radius
-                          fun = mean, # extract the MEAN value from each plot
-                          sp = TRUE) # create spatial object
-class(pmdat2)
-pmdat2@data %>% glimpse
-summary(pmdat2)
-names(pmdat2)[8] <- "pm25"
-pmdat2$pm25      <- ifelse(pmdat2$pm25 < -10, NA, pmdat2$pm25)
-
-spplot(pmdat2, "pm25")
-
-### CALIFORNIA
-
-shape_file <- "gz_2010_06_140_00_500k"
-shape_file_dir <- paste0("plotting/", shape_file)
-raw_tract <- readOGR(dsn = shape_file_dir, layer = shape_file)
-
-# pmdat <- projectRaster(r, crs = crs(raw_tract))
-# crop the data
-pmdat <- crop(r, raw_tract)
-
-class(pmdat)
-pmdat
-head(coordinates(pmdat))
-tail(coordinates(pmdat))
-head(values(pmdat))
-summary(values(pmdat))
-summary(values(pmdat)[which(values(pmdat)>0)])
-
-pmdat2 <- raster::extract(pmdat, # the raster that you wish to extract values from
-                          raw_tract, # a point, or polygon spatial object
-                          buffer = .1, # specify a .5 degree radius
-                          fun = mean, # extract the MEAN value from each plot
-                          sp = TRUE) # create spatial object
-class(pmdat2)
-pmdat2@data %>% glimpse
-summary(pmdat2)
-names(pmdat2)[8] <- "pm25"
-pmdat2$pm25      <- ifelse(pmdat2$pm25 < 0, NA, pmdat2$pm25)
-
-spplot(pmdat2, "pm25")
-
-######################### CALIFORNIA (more)
-
-states <- readShapePoly("plotting/tl_2017_us_state/tl_2017_us_state.shp")
-names(states)
-states@data %>% glimpse
-
-states.ca <- states[states$STATE_ABBR == "CA",]
-bbox(states.ca)
-
-e <- as(extent(-179.23, 179.86, -14.6, 71.44), 'SpatialPolygons')
-# extent format (xmin,xmax,ymin,ymax)
-
-pmdat.ca <- crop(r, e)
-pmdat.ca
-
-pmdat.ca[pmdat.ca < 0] <- NA
-
-library(leaflet); library(rgeos)
-library(leaflet.extras)
-library(rgdal)
-library(mapview); library(webshot)
-
-
-load("plotting/annual_USA_072717.RData")
-dtam.2006 <- na.omit(annual.allobs[which(annual.allobs$year == 2006 & annual.allobs$state.code == "06"), 
-                                   c("latitude", "longitude", "year", "measured")])
-# coordinates(dtam.2006) <- ~ longitude + latitude
-# proj4string(dtam.2006) <- CRS(proj4string(pmdat.ca))
-rm(annual.allobs)
-
-pal <- colorNumeric(rev(brewer.pal(n=11, name = "RdYlGn")), values(pmdat.ca),
-                    na.color = "transparent")
-
-names(providers)
-
-m1 <- leaflet() %>% addProviderTiles(providers$Stamen.TonerLite) %>%
-  addRasterImage(pmdat.ca, colors = pal, opacity = 0.5) %>%
-  addLegend(pal = pal, values = values(pmdat.ca),
-            title = "PM2.5") %>%
-  addCircleMarkers(lng = dtam.2006$longitude, # we feed the longitude coordinates 
-                   lat = dtam.2006$latitude,
-                   radius = 3, 
-                   stroke = FALSE, 
-                   fillOpacity = 0.9, 
-                   color = pal(dtam.2006$measured)) %>%
-  fitBounds(-180.0, 180.0, -15.0, 72.0)
-
-m1
-
-mapshot(m1, file = "mapPM25_CA_new.png")
-
-m2 <- leaflet() %>% addProviderTiles(providers$Stamen.TonerLite) %>%
-  # addRasterImage(pmdat.ca, colors = pal, opacity = 0.3) %>%
-  addLegend(pal = pal, values = values(pmdat.ca),
-            title = "PM2.5") %>%
-  addCircleMarkers(lng = dtam.2006$longitude, # we feed the longitude coordinates 
-                   lat = dtam.2006$latitude,
-                   radius = 4, 
-                   stroke = FALSE, 
-                   fillOpacity = 1, 
-                   color = pal(dtam.2006$measured)) %>%
-  fitBounds(-123.02407, 37.10732, -121.46928, 38.32121)
-
-m2
-
-mapshot(m2, file = "mapPM25_CA_OnlyMonitors.png")
-
-
-## make sure this is the CA raw_tract (i was too lazy to change names...)
-pmdat.ca2 <- raster::extract(pmdat.ca, # the raster that you wish to extract values from
-                             raw_tract, # a point, or polygon spatial object
-                             buffer = .1, # specify a .5 degree radius
-                             fun = mean, # extract the MEAN value from each plot
-                             sp = TRUE) # create spatial object
-class(pmdat.ca2)
-pmdat.ca2@data %>% glimpse
-summary(pmdat.ca2)
-names(pmdat.ca2)[8] <- "pm25"
-pmdat.ca2$pm25      <- ifelse(pmdat.ca2$pm25 < -10, NA, pmdat.ca2$pm25)
-spplot(pmdat.ca2, "pm25")
-
-save(pmdat.ca2, file="PM_Data_CAb.RData")
-
+ca_epa_jan %>% as.data.frame %>% 
+  ggplot() + geom_polygon(data = states, 
+                          aes(x=long, y = lat, group = group), 
+                          fill = "white", 
+                          color="black") +
+  coord_fixed(xlim = c(-124.18, -115.49),  ylim = c(32.64, 41.73), ratio = 1.1) +
+  geom_point(aes(x = Longitude, y = Latitude, color=PM2.5), size = 3, alpha=3/4) +
+  scale_colour_gradientn(colours = terrain.colors(20)) +
+  ggtitle("True PM 2.5 - Jan 2016") + coord_equal() + theme_bw()
