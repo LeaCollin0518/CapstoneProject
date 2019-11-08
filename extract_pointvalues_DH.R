c92e7873a80c1dc9a9ff22b1267838007eb07b71
LeaCollin0518 <leac750@gmail.com>
2019-11-08 14:24:37 -0500

    change to Dalhousie 2010

--- extract_pointvalues_DH.R
+++ extract_pointvalues_DH.R
@@ -1,15 +1,18 @@
 # Script for Extracting Points from ASCII Grid File
 library(raster)
 library(rgdal)
+
+setwd("C:/Users/leac7/Documents/Columbia/Capstone/CapstoneProject")
+
 # Read in .asc file and raster it
-asc_file <- "Annual/GWRwSPEC_PM25_NA_201601_201612-RH35-NoNegs.asc/GWRwSPEC_PM25_NA_201601_201612-RH35-NoNegs.asc"
-asc_file_dir <- paste0("/Users/daniellesu/proj/capstone/CapstoneProject/Data/", asc_file)
+asc_file <- "annual/GWRwSPEC_PM25_NA_201001_201012-RH35-NoNegs.asc"
+asc_file_dir <- paste0("Data/dalhousie_v2/", asc_file)
 raster_file <- raster(asc_file_dir)
 
 # Shape file of tract you want to look at
 # Currently: California
 shape_file <- "gz_2010_06_140_00_500k"
-shape_file_dir <- paste0("/Users/daniellesu/proj/capstone/CapstoneProject/Data/", shape_file)
+shape_file_dir <- paste0("Data/plotting/", shape_file)
 raw_tract <- readOGR(dsn = shape_file_dir, layer = shape_file)
 
 # Crop raster to tract
@@ -23,7 +26,9 @@ point_values <- raster::extract(cropped_raster, # the raster that you wish to ex
 # Create dataframe of coordinates and extracted point values
 point_df <- data.frame(coordinates(cropped_raster), point_values)
 names(point_df) <- c("x", "y", "PM25")
-file_name <- "/Users/daniellesu/proj/capstone/CapstoneProject/Data/dh_ca_annual_2010.csv"
+file_name <- "Data/cali_example/dh_ca_annual_2010.csv"
 write.csv(point_df,
           file = file_name,
           row.names=FALSE)
+
+dh <- read.csv(file_name, header = TRUE)
