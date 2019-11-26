a01277e7b3211cb49342ce6188051d42338ece50
Hammaad Adam <hha2116@columbia.edu>
2019-11-25 22:50:10 -0500

    added gbd processing, minor changes

--- Processing/grid_mapping/process_gbd.R
+++ Processing/grid_mapping/process_gbd.R
@@ -1,8 +1,18 @@
 library(dplyr)
-load("clean_models/pre_clean/GBD2016_PREDPOP_FINAL")
+load("../../Data/grid_mapping/clean_models/pre_clean/GBD2016_PREDPOP_FINAL")
 
-year = 2010
-gbd_year <- mydata2 %>% filter(Country=="USA") %>% rename(x=Longitude, y=Latitude) %>% 
-            select(x,y,paste0("Mean_PM2.5_", year))
-write.csv(gbd_year, paste0("clean_models/gbd_", year, ".csv"))
 
+mydata2 <- mydata2 %>% filter(Country=="USA") %>% rename(x=Longitude, y=Latitude)
+gbd <- mydata2 %>% select(x,y,paste0("Mean_PM2.5_2000")) %>% rename(pm25=`Mean_PM2.5_2000`)
+gbd$year <- 2000
+
+years <- c(2005, 2010:2016)
+
+for(year in years){
+  gbd_year <- mydata2 %>% select(x,y,paste0("Mean_PM2.5_", year))
+  gbd_year$year <- year
+  names(gbd_year) <- names(gbd)
+  gbd <- rbind(gbd, gbd_year)
+}
+
+write.csv(gbd, paste0("../../Data/grid_mapping/clean_models/gbd_2000_2016.csv"))
