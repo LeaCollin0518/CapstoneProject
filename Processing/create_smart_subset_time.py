e53a98583bcd92471255c5f4433cd7a10b4d26f8
LeaCollin0518 <leac750@gmail.com>
2019-12-01 18:26:49 -0500

    data prep scripts for nationwide

--- Processing/create_smart_subset_time.py
+++ Processing/create_smart_subset_time.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
-# coding: utf-8
 
 import pandas as pd
 import numpy as np
 import math
 
-# Nationwide
+
+# # Nationwide
+
+# In[2]:
+
 
 data_dir = '../Data/nationwide/'
-save_dir = '../Data/nationwide_subsets/'
+save_dir = '../Data/nationwide_subsets_v3/'
 
-av_2004 = pd.read_csv(data_dir + 'AV_2004_align.csv')
 
-min_lon = min(av_2004.lon)
-max_lon = max(av_2004.lon)
-min_lat = min(av_2004.lat)
-max_lat = max(av_2004.lat)
+intersecting_av_gbd_scott = pd.read_csv('{}predictions_2010_2016.csv'.format(data_dir))
 
-X_valid = np.asarray(av_2004[["lon", "lat"]].values.tolist()).astype(np.float32)
+one_year = intersecting_av_gbd_scott.loc[intersecting_av_gbd_scott.time == 2010]
+
+min_lon = min(one_year.lon)
+max_lon = max(one_year.lon)
+min_lat = min(one_year.lat)
+max_lat = max(one_year.lat)
+
+X_valid = np.asarray(one_year[["lon", "lat"]].values.tolist()).astype(np.float32)
 X_scale = np.max(X_valid, axis=0) - np.min(X_valid, axis=0)
 
 def round_nearest(x, a):
@@ -61,36 +67,65 @@ def get_splits(X_val, num_splits):
     
     return lon_range, lat_range
 
-lon_range, lat_range = get_splits(X_valid, 49)
+lon_range, lat_range = get_splits(X_valid, 50)
 
-av_2010_2016 = pd.DataFrame(columns=['lon', 'lat', 'pm25', 'time'])
-for i in range(2010, 2017):
-    av_file_name = '{}AV_{}_align.csv'.format(data_dir, i)
-    av_i = pd.read_csv(av_file_name)
-    time_i = np.repeat(i, av_i.shape[0])
-    av_i['time'] = time_i
-    av_2010_2016 = av_2010_2016.append(av_i)
 
-av_2010_2016 = av_2010_2016.loc[av_2010_2016.pm25 != -999.99]
+intersecting_av_gbd_scott = intersecting_av_gbd_scott[['lon', 'lat', 'time', 'pred_AV', 'pred_SC', 'pred_GS', 'pred_CM']]
 
-full_data = pd.DataFrame(columns=['lon', 'lat', 'pm25', 'time'])
-model = av_2010_2016
+full_data = pd.DataFrame(columns=['lon', 'lat', 'time', 'pred_AV', 'pred_SC', 'pred_GS', 'pred_CM'])
+model = intersecting_av_gbd_scott
+all_models = ['AV', 'SC', 'GS', 'CM']
 subset_num = 0
+subset_shapes = []
+
+most_recent_subset = pd.DataFrame(columns=['lon', 'lat', 'pred_AV', 'pred_SC', 'pred_GS', 'pred_CM'])
 
 for i in range(len(lat_range)):
     for j in range(len(lon_range)):
         df_subset = model.loc[(model.lon >= lon_range[j][0]) & (model.lon <= lon_range[j][1]) & (model.lat >= lat_range[i][0]) & (model.lat <= lat_range[i][1])]
-        df_subset = df_subset[['lon', 'lat', 'pm25', 'time']]
+        df_subset = df_subset[['lon', 'lat', 'time', 'pred_AV', 'pred_SC', 'pred_GS', 'pred_CM']]
         if (df_subset.shape[0] != 0):
-            full_data = full_data.append(df_subset)
-            subset_num += 1
-            new_file = '{}AV_2010_2016_align.{}.csv'.format(save_dir, subset_num)
-            print (new_file)
-            df_subset.to_csv(new_file, index = False)
+            if (df_subset.shape[0] > 800):
+                most_recent_subset = df_subset
+                print (most_recent_subset.shape)
+                full_data = full_data.append(most_recent_subset)
+                subset_shapes.append(most_recent_subset.shape[0])
+                subset_num += 1
+                for m in all_models:
+                    pred_name = 'pred_{}'.format(m)
+                    pred_subset = most_recent_subset[['lon', 'lat', 'time', pred_name]]
+                    print (pred_subset.columns)
+                    pred_subset = pred_subset.rename(columns={pred_name: "pm25"})
+                    print (pred_subset.columns)
+                    new_file = '{}{}_2010_2016_align.{}.csv'.format(save_dir, m, subset_num)
+                    print (new_file)
+                    # print (pred_subset.head())
+                    pred_subset.to_csv(new_file, index = False)
+            else:
+                print ("DF Subset: " + str(df_subset.shape))
+                most_recent_subset = most_recent_subset.append(df_subset)
+                print (most_recent_subset.shape)
+                subset_shapes.pop()
+                subset_shapes.append(most_recent_subset.shape[0])
+                full_data = full_data.append(most_recent_subset)
+                for m in all_models:
+                    pred_name = 'pred_{}'.format(m)
+                    pred_subset = most_recent_subset[['lon', 'lat', 'time', pred_name]]
+                    pred_subset = pred_subset.rename(columns={pred_name: "pm25"})
+                    print (pred_subset.columns)
+                    new_file = '{}{}_2010_2016_align.{}.csv'.format(save_dir, m, subset_num)
+                    print (new_file)
+                    # print (pred_subset.head())
+                    pred_subset.to_csv(new_file, index = False) 
 
+
+
+print (len(subset_shapes))
+print (np.min(subset_shapes))
 
 full_data.drop_duplicates(inplace = True)
-print (av_2010_2016.shape[0]/7)
-print (full_data.shape[0]/7)
+# print (av_2010_2016.shape[0]/7)
+print (full_data.shape)
 
-# 1605 subsets
+
+# # 1605 subsets or 1590?
